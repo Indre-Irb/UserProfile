605f27414aacf3170df50b964faf06b3ad037a85
unknown <indre.irbinskaite@gmail.com>
2021-12-02 09:08:04 +0200

    dark mode

--- UserProfile.js
+++ UserProfile.js
@@ -11,38 +11,81 @@ content3.style.display = "none";
 content4.style.display = "none";
 content5.style.display = "none";
 
-menu[0].onclick=()=> {
+menu[0].onclick = () => {
     content1.style.display = "block";
     content2.style.display = "none";
     content3.style.display = "none";
     content4.style.display = "none";
     content5.style.display = "none";
 }
-menu[1].onclick=()=> {
+menu[1].onclick = () => {
     content1.style.display = "none";
     content2.style.display = "block";
     content3.style.display = "none";
     content4.style.display = "none";
     content5.style.display = "none";
 }
-menu[2].onclick=()=> {
+menu[2].onclick = () => {
     content1.style.display = "none";
     content2.style.display = "none";
     content3.style.display = "block";
     content4.style.display = "none";
     content5.style.display = "none";
 }
-menu[3].onclick=()=> {
+menu[3].onclick = () => {
     content1.style.display = "none";
     content2.style.display = "none";
     content3.style.display = "none";
     content4.style.display = "block";
     content5.style.display = "none";
 }
-menu[4].onclick=()=> {
+menu[4].onclick = () => {
     content1.style.display = "none";
     content2.style.display = "none";
     content3.style.display = "none";
     content4.style.display = "none";
     content5.style.display = "flex";
+}
+
+const workCards = document.querySelectorAll(".workCard")
+const workModal = document.querySelector(".workModal")
+const close = document.getElementsByClassName("close")[0]
+const darkMode = document.querySelector(".darkMode")
+const addDark = document.querySelectorAll(".addDark")
+
+
+let change = false
+
+workModal.style.display = "none";
+
+workCards.forEach((x, index) => {
+    x.onclick = () => {
+        workModal.style.display = "flex";
+    }
+})
+
+close.onclick = () => {
+    workModal.style.display = "none";
+}
+
+
+darkMode.onclick = () => {
+    changeColor()
+}
+
+function changeColor() {
+    addDark.forEach(x => {
+        console.log(x)
+        if (!change) {
+            document.body.style.background = "black"
+            x.style.background = "rgb(91,90,90)"
+            x.style.color = "lightgray"
+            x.style.borderRadius = `10px/10px`
+        } else {
+            document.body.style.background = "rgba(229, 234, 234, 0.79)"
+            x.style.background = "white"
+            x.style.color = "#505050"
+        }
+    })
+    change = !change
 }
